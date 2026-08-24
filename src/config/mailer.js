6cac296d3d2715721b7ab0ed8bdbba88d67e10e6
Goutam Kumar <goutamkumar42010no@gmail.com>
2026-08-24 23:33:30 +0530

    update mailer

--- src/config/mailer.js
+++ src/config/mailer.js
@@ -5,6 +5,7 @@ export const transporter = nodemailer.createTransport({
   host: "smtp.gmail.com",
   port: 465,
   secure: true, // use SSL
+  family: 4,
   auth: {
     user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_PASS,
